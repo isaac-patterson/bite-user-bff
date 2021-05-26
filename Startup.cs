fc6acc092ed57ab4b99e522f47a6e60e2af80f4a
Isaac P <nzipatterson@gmail.com>
2021-05-26 21:53:40 +1200

    Add path prefix

--- Startup.cs
+++ Startup.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -39,6 +40,7 @@ namespace veni_bff
              //Accept All HTTP Request Methods from all origins
             app.UseCors(builder =>builder.AllowAnyHeader().AllowAnyOrigin().AllowAnyMethod());
 
+            app.UsePathBase(new PathString("/user"));
             app.UseRouting();
             app.UseEndpoints(endpoints =>
             {
